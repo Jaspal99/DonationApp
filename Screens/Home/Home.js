04774a3f07435b70f024eacdac0bc013a35dbcc1
Jaspal <khalsajaspalsingh6@gmail.com>
2023-09-23 16:19:20 +0530

    component for button and tab

--- Screens/Home/Home.js
+++ Screens/Home/Home.js
@@ -1,12 +1,27 @@
 import {SafeAreaView, Text, View} from 'react-native';
 import globalStyle from '../../Assets/Styles/GlobalStyles';
+import Header from '../../Components/Header/Header';
+import Button from '../../Components/Button/Button';
+import Tab from '../../Components/Tab/Tab';
 
 export default function Home() {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
-      <View>
-        <Text>Home Screen</Text>
-      </View>
+      <Header title={'Jaspal S.'} type={1} />
+      <Button
+        title={'Donate'}
+        onPress={() => {
+          console.log('Pressed me');
+        }}
+      />
+      <Tab title={'Tab1'} isInactive={true} />
+      <Tab
+        title={'Tab2'}
+        onPress={() => {
+          console.log('Pressed Tab');
+        }}
+      />
+      {/* <Button title={'Donate'} isDisabled={true} /> */}
     </SafeAreaView>
   );
 }
