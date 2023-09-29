6e305678b5916501fadcf608ee56bab4b210dbcc
Jaspal <khalsajaspalsingh6@gmail.com>
2023-09-29 20:43:55 +0530

    initialized test

--- redux/reducers/User.js
+++ redux/reducers/User.js
@@ -8,6 +8,7 @@ const initialState = {
     'https://cdn.dribbble.com/users/1577045/screenshots/4914645/media/028d394ffb00cb7a4b2ef9915a384fd9.png?compress=1&resize=400x300&vertical=top',
 };
 
+const test = 1;
 export const User = createSlice({
   name: 'user',
   initialState: initialState,
